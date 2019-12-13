f6b2d4591865aecd46d1237312ea27ad363de22b
Joshua Rieth <joshua.m.rieth@gmail.com>
2019-12-12 20:54:38 -0500

    Completed Step 3

---
+++ user-onboarding/src/components/UserForm/UserForm.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+function UserForm({ values, errors, touched, status }) {
+	console.log('values', values);
+	console.log('errors', errors);
+	console.log('touched', touched);
+
+	const [ users, setUsers ] = useState([]);
+
+	return (
+		<div className="user-form">
+			<Form>
+				<label htmlFor="name">
+					Name
+					<Field id="name" type="text" name="name" placeholder="Name" />
+					{touched.name && errors.name && <p className="errors">{errors.name}</p>}
+				</label>
+				<label htmlFor="email">
+					Email
+					<Field id="email" type="email" name="email" placeholder="Email" />
+					{touched.email && errors.email && <p className="errors">{errors.email}</p>}
+				</label>
+				<label htmlFor="password">
+					Password
+					<Field id="password" type="password" name="password" placeholder="Password" />
+					{touched.password && errors.password && <p className="errors">{errors.password}</p>}
+				</label>
+				<label htmlFor="terms">
+					<Field id="terms" type="checkbox" name="terms" required />
+					Accept Terms of Service
+				</label>
+				<button type="submit">Submit</button>
+			</Form>
+		</div>
+	);
+}
+
+const FormikUserForm = withFormik({
+	mapPropsToValues(props) {
+		return {
+			name: props.name || '',
+			email: props.email || '',
+			password: props.password || '',
+			terms: props.terms || false
+		};
+	},
+	validationSchema: Yup.object().shape({
+		name: Yup.string().required(),
+		email: Yup.string().required(),
+		password: Yup.string().required()
+	}),
+	handleSubmit(values, { setStatus, resetForm }) {
+		console.log('values', values);
+		axios
+			.post('https://reqres.in/api/users', values)
+			.then((res) => {
+                console.log('success', res);
+                setStatus(res.data)
+				resetForm();
+			})
+			.catch((err) => console.log('error', err));
+	}
+})(UserForm);
+
+export default FormikUserForm;
